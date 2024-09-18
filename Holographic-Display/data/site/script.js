e7d5f6773d6de1adb34dbe30f0c945b30da3645f
lostMia <testemail429455@gmail.com>
2024-09-18 17:01:50 +0200

    Did some Website work

--- Holographic-Display/data/site/script.js
+++ Holographic-Display/data/site/script.js
@@ -1,5 +1,6 @@
 const dropZone = document.getElementById('drop_zone');
 const progressBar = document.getElementById('progressBar');
+const maxUploadSize = 1024 * 1024;
 
 // Slider and Manual Input Synchronization
 const slider = document.getElementById('slider');
@@ -41,6 +42,11 @@ function handleFiles(files) {
     const formData = new FormData();
     formData.append('file', file);
 
+    if (file.size > maxUploadSize) {
+      alert("File too big!");
+      return;
+    }
+
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/upload', true);
     xhr.upload.onprogress = (event) => {
@@ -78,4 +84,28 @@ document.querySelectorAll('#dataForm input, #dataForm select').forEach(function(
   element.addEventListener('input', function(event) {
     sendData(event.target);
   });
+});
+
+
+
+document.querySelectorAll('.item-container').forEach(container => {
+    const box = container.querySelector('.item');
+
+    container.addEventListener('mousemove', function (event) {
+        const boxRect = box.getBoundingClientRect();
+        const centerX = boxRect.left + boxRect.width / 2;
+        const centerY = boxRect.top + boxRect.height / 2;
+        const offsetX = (event.clientX - centerX) / boxRect.width * 2;
+        const offsetY = (event.clientY - centerY) / boxRect.height * 2;
+
+        const rotateX = offsetY * 15;
+        const rotateY = offsetX * -15;
+        const translateZ = 30; // Adjust the 30 value for more or less pop out
+
+        box.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(${translateZ}px)`;
+    });
+
+    container.addEventListener('mouseleave', function () {
+        box.style.transform = `rotateX(0deg) rotateY(0deg) translateZ(0)`;
+    });
 });
