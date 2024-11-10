f50491e8bedfdc6a3490a78198729694206b5b15
lostMia <testemail429455@gmail.com>
2024-11-10 12:27:42 +0100

    The fuck is a 'Conversionmatrix-Converter'?

--- Conversionmatrix-Generator/flake.nix
+++ Conversionmatrix-Generator/flake.nix
@@ -6,23 +6,21 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
-      opencvGtk = pkgs.opencv.override (old : { enableGtk2 = true; });
     in
     {
       packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        pname = "conversion-matrix";
+        pname = "conversionmatrix-generator";
         version = "0.1.0";
 
         src = ./.;
 
         buildInputs = [ 
           pkgs.gcc
-          opencvGtk
         ];
 
         # Define build steps
         buildPhase = ''
-          g++ -o main src/main.cpp
+          g++ -o conversionmatrix-generator src/main.cpp
         '';
 
         # Define install steps
