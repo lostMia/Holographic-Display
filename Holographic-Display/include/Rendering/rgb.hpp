b65042d1160d86d964fb42d34cf690315f0fda3d
lostmia <testemail429455@gmail.com>
2025-02-28 14:49:41 +0100

    Can't forget about the very essential file descriptions

--- Holographic-Display/include/Rendering/rgb.hpp
+++ Holographic-Display/include/Rendering/rgb.hpp
@@ -1,3 +1,12 @@
+/*
+ * @file rgb.hpp
+ * @authors mia
+ * @brief defines the RGB struct.
+ * @version 0.1.0
+ * @date 2025-02-28
+ *
+ * Copyright Deimo Elektronik GmbH (c) 2025
+*/
 
 #pragma once
 
--- Holographic-Display/src/Rendering/rgb.cpp
+++ Holographic-Display/src/Rendering/rgb.cpp
@@ -1,9 +1,18 @@
+/*
+ * @file rgb.cpp
+ * @authors mia
+ * @brief defines the RGB static members.
+ * @version 0.1.0
+ * @date 2025-02-28
+ *
+ * Copyright Deimo Elektronik GmbH (c) 2025
+*/
 
-#include "rgb.hpp"
+
+#include "Rendering/rgb.hpp"
 
 const RGB RGB::Red      = RGB(255, 0, 0);
 const RGB RGB::Green    = RGB(0, 255, 0);
 const RGB RGB::Blue     = RGB(0, 0, 255);
 const RGB RGB::White    = RGB(255, 255, 255);
 const RGB RGB::Black    = RGB(0, 0, 0);
-
